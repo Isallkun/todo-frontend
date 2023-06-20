cbe6711aedb4082709f5462a0ac0fa09dd65978d
santriinsyaf <aswadalansori@gmail.com>
2023-06-21 03:48:02 +0700

    create todo app with react

---
+++ src/components/Author/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { AuthorContainer } from "./styles";
+
+function Author() {
+  return (
+    <AuthorContainer>
+      Created by{" "}
+      <a href="https://isallkun.my.id" target="_blank" rel="noopener noreferrer">
+        Isallkun
+      </a>
+    </AuthorContainer>
+  );
+}
+
+export default Author;
---
+++ src/components/Author/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const AuthorContainer = styled.span`
+  font-size: 1rem;
+  display: flex;
+  justify-content: center;
+
+  a {
+    cursor: pointer;
+    margin: 0 0.2rem;
+  }
+`;
